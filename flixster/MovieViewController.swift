d96c63dea49ea0fa3f0e3292e0e97eecae0fd95c
Paul Newling - iMac <PaulNewling@Gmail.com>
2021-02-13 13:18:12 -0800

    Initi Commit

--- flixster/MovieViewController.swift
+++ flixster/MovieViewController.swift
@@ -13,7 +13,7 @@ class MovieViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
