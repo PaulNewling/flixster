f25cae22e04a311bc939f0fc702bc51abb6f62b6
Paul Newling <PaulNewling@Gmail.com>
2021-02-15 10:44:42 -0800

    Assingment 2 started

---
+++ flixster/MovieDetailsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MovieDetailsViewController.swift
+//  flixster
+//
+//  Created by Paul Newling on 2/15/21.
+//  Copyright © 2021 Paul Newling. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    var movie: [String:Any]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
