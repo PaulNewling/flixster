0a49c5f6aab7a3209d3b183c1430d4ee82ef1332
Paul Newling <PaulNewling@Gmail.com>
2021-02-15 12:39:54 -0800

    flixster collection view complete

--- flixster/MovieDetailsViewController.swift
+++ flixster/MovieDetailsViewController.swift
@@ -7,17 +7,40 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieDetailsViewController: UIViewController {
 
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
     var movie: [String:Any]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let basePosterURL = "https://image.tmdb.org/t/p/w185"
+        let posterpath = movie["poster_path"] as! String
+        let posterURL = URL(string: basePosterURL + posterpath)
+        
+        posterView.af_setImage(withURL: posterURL!)
+        
+        let baseBackdropURL = "https://image.tmdb.org/t/p/w780"
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropURL = URL(string: baseBackdropURL + backdropPath)
+        
+        backdropView.af_setImage(withURL: backdropURL!)
+        
+        
         
     }
-    
 
     /*
     // MARK: - Navigation
---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Paul Newling on 2/15/21.
+//  Copyright © 2021 Paul Newling. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
